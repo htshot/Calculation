f76e579eacce1d3b1f3f322ae64b60a65e4b98e6
htshot <chjins05@gmail.com>
2023-09-08 20:50:53 +0900

    class설정

--- script.js
+++ script.js
@@ -1,1 +1,17 @@
-console.log(1);
+class Calculator{
+
+}
+
+const cal = new Calculator();
+
+const $previousPreview = document.querySelector("[data-previous-preview]");
+const $currentPreview = document.querySelector("[data-previous-current]");
+console.log($previousPreview)
+console.log($currentPreview)
+
+
+// function Calculator () {
+
+// }
+
+// const cal = new Calculator()
