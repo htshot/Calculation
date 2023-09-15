f5374c1ec5420fdd85ca4e1073d3711c538cec84
htshot <chjins05@gmail.com>
2023-09-15 23:47:02 +0900

    마무리

--- readme.md
+++ readme.md
@@ -5,11 +5,12 @@
 - [x] data 속성 추가
 - [x] calculator class 만들기
 - [x] 계산기 기능 관련 DOM 셋팅
-- [x] 숫자 버튼 기능 구현
-- [x] 사칙연산 버튼 기능 구현
+- [x] 숫자 버튼 기능(onPressNumber) 출력 구현
+- [x] 사칙연산 버튼 기능(onPressOperation) 출력 구현
 
 ### 2주차
-- [ ] onEqual 기능 구현
-- [ ] onReset 기능 구현
-- [ ] onDelete 기능 구현
-- [ ] 데이터 벨리데이션 추가
+- [x] 데이터 벨리데이션 1차 추가
+- [x] onEqual 기능 구현
+- [x] onReset 기능 구현
+- [x] onDelete 기능 구현
+- [x] onEqual 리펙토링
--- script.js
+++ script.js
@@ -1,49 +1,139 @@
-class Calculator{
+class Calculator {
     $previousPreview
     $currentPreview
+    previousOperation = ""
+    currentOpenration = ""
 
-    constructor($previousPreview, $currentPreview ){
+    constructor($previousPreview, $currentPreview) {
         this.$previousPreview = $previousPreview
         this.$currentPreview = $currentPreview
     }
 
     onPressNumber(number) {
-        // TODO: 벨리데이션 
-        this.$currentPreview.textContent += number
+       // 소수점 입력시 현재 입력 받은 데이터가 없는 경우
+       if (number === "." && this.$currentPreview.textContent.length < 1) {
+         return 
+       }
+
+       this.$currentPreview.textContent += number
     }
 
-    onPressOperation(operation){
+    onPressOperation(operation) {
+        // 연산기호 입력시 현재 입력 받은 데이터가 없는 경우
+        if (this.$currentPreview.textContent.length < 1) {
+            return 
+        }
+        this.previousOperation = operation
         this.$previousPreview.textContent = `${this.$currentPreview.textContent} ${operation}`
-        this.$currentPreview.textContent  = ''
+        this.$currentPreview.textContent = ''
+    }
+
+    onEqual() {
+        if (
+            this.$currentPreview.textContent.length < 1 ||
+            this.$previousPreview.textContent.length < 1 ||
+            this.previousOperation.length < 1
+        ) {
+            return
+        }
+
+        let result = 0
+
+        switch (this.previousOperation) {
+            case "+":
+                result = this.handlePlus()
+                break
+            case "-":
+                result = this.handleMinus()
+                break
+            case "*":
+                result = this.handleMultiply()
+                break
+            case "÷":
+                result = this.handleDivide()
+                break
+            default:
+                break
+        }
+        this.$currentPreview.textContent = result.toString()
+        this.$previousPreview.textContent = ""
+        this.currentOperation = ""
+    }
+
+    handlePlus() {
+        return (
+            Number(this.$previousPreview.textContent.split(" ")[0])
+            + Number(this.$currentPreview.textContent)
+        )
+    }
+
+    handleMinus() {
+        return (
+            Number(this.$previousPreview.textContent.split(" ")[0])
+            - Number(this.$currentPreview.textContent)
+        )
+    }
+
+    handleMultiply() {
+        return (
+            Number(this.$previousPreview.textContent.split(" ")[0])
+            * Number(this.$currentPreview.textContent)
+        )
+    }
+
+    handleDivide() {
+        return (
+            Number(this.$previousPreview.textContent.split(" ")[0])
+            / Number(this.$currentPreview.textContent)
+        )
+    }
+
+    onReset() {
+        this.$currentPreview.textContent = ""
+        this.$previousPreview.textContent = ""
+        this.previousOperation = ""
+        this.currentOpenration = ""
+    }
+
+    onDelete() {
+        if (this.$currentPreview.textContent < 1) {
+            return
+        }
+
+        this.$currentPreview.textContent = this.$currentPreview.textContent.slice(0, -1)
     }
 }
 
-
-
 // 값 표시
-const $previousPreview = document.querySelector("[data-previous-preview]");
-const $currentPreview = document.querySelector("[data-previous-current]");
+const $previousPreview = document.querySelector('[data-previous-preview]')
+const $currentPreview = document.querySelector('[data-current-preview]')
 
 // 사칙연산
-const $plus= document.querySelector("[data-btn-plus]");
-const $minus = document.querySelector("[data-btn-minus]");
-const $divide = document.querySelector("[data-btn-divide]");
-const $multiply = document.querySelector("[data-btn-multiply]");
-const $equal = document.querySelector("[data-btn-equal]");
+const $plus = document.querySelector('[data-btn-plus]')
+const $minus = document.querySelector('[data-btn-minus]')
+const $multiply = document.querySelector('[data-btn-multiply]')
+const $divide = document.querySelector('[data-btn-divide]')
+const $eqaul = document.querySelector('[data-btn-eqaul]')
+
+// 리셋, 삭제
+const $reset = document.querySelector('[data-btn-reset]')
+const $delete = document.querySelector('[data-btn-delete]')
 
 // 숫자, 연산
-const $numbers = document.querySelectorAll('[data-btn-Number]')
+const $numbers = document.querySelectorAll('[data-btn-number]')
 const $operations = document.querySelectorAll('[data-btn-operation]')
 
-$numbers.forEach(($number)=>{
-    $number.addEventListener("click", (e)=>{
-        cal.onPressNumber(e.target.textContent);
+const cal = new Calculator($previousPreview, $currentPreview)
+
+$numbers.forEach(($number) => {
+    $number.addEventListener("click", (e) => {
+        cal.onPressNumber(e.target.textContent)
     })
 })
 
-$operations.forEach(($operation)=>{
-    $operation.addEventListener("click", (e)=>{
-        switch ($operation) {
+$operations.forEach(($operation) => {
+    $operation.addEventListener("click", (e) => {
+       switch ($operation) {
             case $plus:
                 cal.onPressOperation("+")
                 break;
@@ -56,16 +146,14 @@ $operations.forEach(($operation)=>{
             case $divide:
                 cal.onPressOperation("÷")
                 break;
-            case $equal:
-                //
+            case $eqaul:
+                cal.onEqual()
                 break;
             default:
                 break;
-        }
-        // cal.onPressOperation(e.target.textContent);
+       }
     })
 })
 
-
-
-const cal = new Calculator($previousPreview,$currentPreview);
+$reset.addEventListener("click", (e) => cal.onReset())
+$delete.addEventListener("click", (e) => cal.onDelete())
